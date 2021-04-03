79321dbb9aaa34fee97ef45c51394723f748108c
Nestor <nestoroprysk@gmail.com>
2021-04-03 16:34:27 +0300

    Adding expenses

--- expenses/fn.go
+++ expenses/fn.go
@@ -1,31 +1,11 @@
 package expenses
 
 import (
-	"log"
 	"net/http"
 
-	"github.com/nestoroprysk/TelegramBots/util"
+	"github.com/nestoroprysk/TelegramBots/admin"
 )
 
 func HandleExpenses(w http.ResponseWriter, r *http.Request) {
-	// Parse incoming request
-	var update, err = util.ParseTelegramRequest(r)
-	if err != nil {
-		log.Printf("error parsing update, %s", err.Error())
-		return
-	}
-
-	text, err := util.SendSQLRequest(update.Message.Text)
-	if err != nil {
-		log.Printf("error interacting with SQL, %s", err.Error())
-		text = err.Error()
-	}
-
-	// Send the SQL response back to Telegram
-	response, err := util.SendTextToTelegramChat(update.Message.Chat.Id, text)
-	if err != nil {
-		log.Printf("got error %s from telegram, reponse body is %s", err.Error(), response)
-	} else {
-		log.Printf("response %s successfuly distributed to chat id %d", update.Message.Text, update.Message.Chat.Id)
-	}
+	admin.HandleAdminSQL(w, r)
 }
