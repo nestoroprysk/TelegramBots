3239f59ffece6aa3ccc6d64394e1c69fed8029c0
Nestor <nestoroprysk@gmail.com>
2021-04-03 15:52:59 +0300

    Trying to add expenses

--- expenses/fn.go
+++ expenses/fn.go
@@ -6,6 +6,6 @@ import (
 	"github.com/nestoroprysk/TelegramBots/admin"
 )
 
-func HandleAdminSQL(w http.ResponseWriter, r *http.Request) {
+func HandleExpenses(w http.ResponseWriter, r *http.Request) {
 	admin.HandleAdminSQL(w, r)
 }
