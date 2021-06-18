fc5fd17aac5639287aac002737845bbf347b0000
Nestor <nestoroprysk@gmail.com>
2021-06-18 05:36:05 +0300

    Printing the secret (not really)

--- admin.go
+++ admin.go
@@ -1,11 +1,14 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/nestoroprysk/TelegramBots/internal/cmd"
 )
 
 func Admin(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("%s", os.Getenv("BOT_SQL_CONNECTION_NAME"))
 	cmd.Admin(w, r)
 }
