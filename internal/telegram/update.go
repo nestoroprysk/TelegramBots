9afc8f4ccc2e0e2bdaa372dc3c153b35fcfaf391
Nestor <nestoroprysk@gmail.com>
2021-06-26 17:03:38 +0300

    Fixing input

--- internal/telegram/update.go
+++ internal/telegram/update.go
@@ -9,13 +9,13 @@ import (
 // Update is a Telegram object that the handler receives every time a user interacts with the bot.
 type Update struct {
 	Message Message `json:"message" validate:"required"`
-	From    From    `json:"from", validate:"required"`
 }
 
 // Message is a Telegram object that can be found in an update.
 type Message struct {
 	Text string `json:"text" validate:"required"`
 	Chat Chat   `json:"chat" validate:"required"`
+	From From   `json:"from", validate:"required"`
 }
 
 // From is a Telegram object that indicates a user that sends a message.
