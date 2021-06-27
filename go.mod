8b3b4c8b2fd12583c3428e7c4ea34544d5d3b0d5
Nestor <nestoroprysk@gmail.com>
2021-06-27 23:17:00 +0300

    Adding validator tests and expanding the cloudbuild

--- go.mod
+++ go.mod
@@ -5,6 +5,10 @@ go 1.13
 require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/jedib0t/go-pretty/v6 v6.2.2
+	github.com/onsi/ginkgo v1.16.4
+	github.com/onsi/gomega v1.13.0
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+	golang.org/x/tools v0.1.4 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2
 )
---
+++ internal/validator/validator_test.go
@@ -0,0 +1,49 @@
+package validator_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+
+	"github.com/nestoroprysk/TelegramBots/internal/validator"
+)
+
+func TestValidator(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Validator Suite")
+}
+
+type Test struct {
+	RequiredString string `json:"text" validate:"required"`
+	PositiveInt    int    `json:"id" validate:"gt=0"`
+	OptionalString string `json:"text"`
+}
+
+var _ = DescribeTable("Validates the test struct", func(t Test, succeed bool) {
+	v := validator.New()
+	result := v.Struct(t)
+	if succeed {
+		Expect(result).To(Succeed())
+	} else {
+		Expect(result).NotTo(BeNil())
+	}
+},
+	Entry("Returns nil when all is fine",
+		Test{RequiredString: "s", PositiveInt: 1},
+		true, /* succeed */
+	),
+	Entry("Returns nil when optional string is indicated as well",
+		Test{RequiredString: "s", PositiveInt: 1, OptionalString: "s"},
+		true, /* succeed */
+	),
+	Entry("Errors if positive int is not positive",
+		Test{RequiredString: "s", PositiveInt: -1},
+		false, /* succeed */
+	),
+	Entry("Returns if required string is empty",
+		Test{RequiredString: "", PositiveInt: 1},
+		false, /* succeed */
+	),
+)
